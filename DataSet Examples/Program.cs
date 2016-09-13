5151afe678646f7b399b7a08f66d174b8a4700d0
Daedalus2051 <Daedalus2051@TALYN>
2016-09-12 22:14:02 -0400

    Initial commit

---
+++ DataSet Examples/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DataSet_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataSet ds = new DataSet("myDataSet");
+            DataTable t = new DataTable("table1");
+            DataColumn c = new DataColumn("myColumnOfData");
+            DataColumn c2 = new DataColumn("MoreData");
+            t.Columns.Add(c);
+            t.Columns.Add(c2);
+            ds.Tables.Add(t);
+
+            
+            DataRow r;
+            for (int i = 0; i < 20; i++)
+            {
+                r = t.NewRow();
+                r[0] = "Data" + i;
+                r[1] = "More_data_" + i;
+                t.Rows.Add(r);
+            }
+
+            Console.WriteLine("The dataset contains the following: ");
+            Console.WriteLine("Dataset name: {0}", ds.DataSetName);
+            
+            int tabcounter = 0, colcounter = 0;
+            
+            foreach (DataTable tab in ds.Tables)
+            {
+                tabcounter++;
+                Console.WriteLine("Table[{0}] name: {1}", tabcounter, t.TableName);
+                foreach(DataColumn col in tab.Columns)
+                {
+                    colcounter++;
+                    Console.WriteLine("\tColumn[{0}] name: {1}", colcounter, col.ColumnName);
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Table data:");
+            //Headers
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (DataColumn col in ds.Tables[0].Columns)
+            {
+                Console.Write("[{0}]\t", col.ColumnName);
+            } Console.WriteLine("");
+            Console.ResetColor();
+            /*
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                for (int i = 0; i < ds.Tables[0].Columns.Count; i++)
+                {
+                    Console.Write("{0}\t", row[i]);
+                } Console.WriteLine("");
+            }
+            */
+            
+            for (int rownum = 0; rownum < ds.Tables[0].Rows.Count; rownum++)
+            {
+                for (int i = 0; i < ds.Tables[0].Columns.Count; i++)
+                {
+                    DataRow cur_row = ds.Tables[0].Rows[rownum];
+                    Console.Write("{0}\t", cur_row[i]);
+                } 
+                Console.WriteLine("");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Drawing_Testbed/Drawing_Testbed/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing_Testbed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g;
+
+            PaintEventArgs pea = new PaintEventArgs(g, new Rectangle());
+
+            DrawLinesPoint(pea);
+        }
+
+        public void DrawLinesPoint(PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.Black, 3);
+
+            Point[] points =
+            {
+                new Point(10,10),
+                new Point(10,100),
+                new Point(200,50),
+                new Point(250,300)
+            };
+
+            e.Graphics.DrawLines(p, points);
+        }
+    }
+}
